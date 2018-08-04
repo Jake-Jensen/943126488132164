62c790eda63ee52d5e4a2e6203d5822a1f0493a5
Elijah Bush <sageqa@splitsecondpro.site>
2018-08-04 17:53:11 -0400

    Added Level 2
    Added Level 2 and did various balance changes.
    
    Important Changes:
    - Buffed Basic Tower damage from 50 ->65
    - Increased amount of money given per kill for easy
    - Added medium values
    - Modified Spawn timing
    - Slightly modified tower lock-on time
    - Added medium button functionality

--- Intermediate/Build/Win64/UE4/Development/ReVolt_Alpha/PCH.ReVolt_Alpha.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/DevOps/Documents/Unreal Projects/ReVolt_Alpha/Intermediate/Build/Win64/UE4/Development/ReVolt_Alpha/PCH.ReVolt_Alpha.h"
--- Intermediate/Source/ReVolt_Alpha.cpp
+++
@@ -1,4 +0,0 @@
-#include "CoreTypes.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, ReVolt_Alpha, "ReVolt_Alpha");
